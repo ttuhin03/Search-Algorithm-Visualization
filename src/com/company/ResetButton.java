ef4e92bc1639eb4922746c422606b64a6517e63a
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-11-03 00:27:16 +0100

    Refacoring - ResetButton als eigen Klasse hinzugefügt. Ist übersichtlicher und mehr OO.

---
+++ src/com/company/ResetButton.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JButton {
+
+    private gui_test gui;
+
+    public ResetButton(gui_test progam){
+        super("Reset");
+
+        gui = progam;
+
+
+        addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e1){
+                gui.setStartpointButton = false;
+                gui.setEndpointButton = false;
+                gui.setWallButton = false;
+                gui.setDeleteButton = false;
+
+                //Setzt alle Felder auf ihren Startzustand (Leeres Feld) zurück
+                gui.resetAllFelder();
+
+                gui.endPointXPos = -42;
+                gui.endPointYPos = -42;
+                gui.startPointXPos = -42;
+                gui.startPointYPos = -42;
+                gui.globalIsEndpointSet = false;
+                gui.globalIsStartpointSet = false;
+
+            } } );
+    }
+}
