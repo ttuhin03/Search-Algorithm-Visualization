390f530e4133abeaf9723f440fabc6290c309e14
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-11-08 20:07:28 +0100

    Added Example Button-Image and used it in RunButton.java to use it in all the Buttons with a better Button-Image/ Beispiel Knopf-Bild hinzugefügt und in RunButton.java benutzt, um es später in allen Buttons mit einem besseren Button-Image zu benutzen.

--- src/com/company/RunButton.java
+++ src/com/company/RunButton.java
@@ -1,16 +1,28 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class RunButton extends JButton{
 
     private gui_test gui;
 
     public RunButton(gui_test progam){
-        super("Run");
+        //super("Run");
+        super(new ImageIcon(new ImageIcon("src/com/company/bilder/Screenshot 2022-11-08 195052.jpg").getImage().getScaledInstance(130, 40, Image.SCALE_DEFAULT)));
+
         gui = progam;
+
+        //Deletes all Borders and Background from Button
+        //Löscht alle Bilder und den Hintergrund vom Knopf
+        setOpaque(false);
+        setFocusPainted(false);
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+        setBorder(BorderFactory.createEmptyBorder(0,0,0,0)); // Especially important
 
         this.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e5){
@@ -42,6 +54,19 @@ public class RunButton extends JButton{
                 }
 
             } } );
+
+        }
+
+    public Image getScaledImage(Image srcImg, int w, int h){
+        BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+
+        return resizedImg;
+
 
     }
 
