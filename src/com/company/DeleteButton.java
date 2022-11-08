4873e195d8fc868de370cedce837fc1b354480d1
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-11-08 20:37:39 +0100

    Added Example Code for changing the Button when the Mouse is hovering above it/ Beispiel Code um den Knopf zu verändern, wenn die Maus über dem Knopf hovert.

--- src/com/company/DeleteButton.java
+++ src/com/company/DeleteButton.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +12,16 @@ public class DeleteButton extends JButton {
     public DeleteButton(gui_test program){
         super("Delete");
         gui = program;
+
+        this.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                setBackground(Color.GREEN);
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                setBackground(Color.BLUE);
+            }
+        });
 
         this.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e4){
