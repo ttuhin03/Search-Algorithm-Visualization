614295839dc604d9138b6ade4274a1114c5973ab
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-11-10 16:05:36 +0100

    Removed Imports, wich are not needed / Imports gelöscht, welche nicht benötigt werden

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,12 @@
 package com.company;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
